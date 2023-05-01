e0c2000c1acc0d00c8497c88d047033f752a25fd
T0HBA <121461213+T0HBA@users.noreply.github.com>
2023-05-01 13:12:15 +0500

    Add files via upload

---
+++ Cyclone.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Cyclone for celebrate 🥂🥂🥳
+// @version      1.0.0
+// @description  Free Tanki Online Cheat for Celebrate 🥂
+// @author       TOHBA
+// @match        https://*.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui_impl.umd.js
+
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://raw.githubusercontent.com/T0HBA/shizoval/main/Cyclone.js',
+    nocache: true,
+    onload: data => eval(data.responseText)
+})
+
+//You don't need any clan to Celebrate this hack 🥂🥂
+//Easy to Celebrate and to Win and to Destroy 🥂🥂
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# shizoval
-shizoval
+# Cyclone
+
+You don't need any clan to use this hack.
+
+Discord: **MR.HACKER_ATTA#5989** | Discord Server(https://discord.gg/WaRDA3bY)
+
+![Cyclone](https://github.com/T0HBA/shizoval/raw/main/Cyclone.png)
+
+## Installation
+
+**1.** Install [Voilentmonkey](https://violentmonkey.github.io/)
+
+**2.** Install [Script](https://raw.githubusercontent.com/T0HBA/shizoval/main/Cyclone.user.js)
+
+## Control
+
+`INSERT`, `NumPad 0`, `/` -
